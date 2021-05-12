2463b58ffd49147d4a49488a2869e450283d3132
petr <petr2768@yandex.ru>
2021-05-12 19:06:01 +0300

    вариант говнокод

--- src/ru/mycompany/Main.java
+++ src/ru/mycompany/Main.java
@@ -21,18 +21,13 @@ public class Main {
 
             if ((x = test(array[0])) != 0 && (y = test(array[2])) != 0) {
                 int res = result(array[1], x, y);
+                System.out.println(resRome(res));
 
-                for (Map.Entry<String, Integer> pair : map.entrySet()) {
-                    if (pair.getValue() == res) {
-                        System.out.println(pair.getKey());
-                    }
-                }
             } else {
                 x = Integer.parseInt(array[0]);
                 y = Integer.parseInt(array[2]);
                 System.out.println(result(array[1], x, y));
             }
-
         }
     }
 
@@ -43,6 +38,15 @@ public class Main {
             }
         }
         return 0;
+    }
+
+    public static String testRome(int number){
+        for (Map.Entry<String, Integer> pair : map.entrySet()){
+            if(number == pair.getValue()){
+                return pair.getKey();
+            }
+        }
+        return "";
     }
 
     public static int result(String sign, int x, int y){
@@ -68,5 +72,95 @@ public class Main {
                 map.put(n[0], Integer.parseInt(n[1]));
             }
         }
+    }
+
+    public static String resRome(int res){
+        if(res <= 10){
+            if(res == 10){
+                return "X";
+            } else {
+                int first = res / 10;
+                int second = res % 10;
+                String x = testRome(first);
+                String y = testRome(second);
+                return x + y;
+            }
+        } else if (res <= 20){
+            if(res == 20){
+                return "XX";
+            } else {
+                int first = 10;
+                int second = res % 10;
+                String x = testRome(first);
+                String y = testRome(second);
+                return x + y;
+            }
+        } else if (res <= 30){
+            if(res == 30){
+                return "XXX";
+            } else {
+                String x = "XX";
+                int second = res % 10;
+                String y = testRome(second);
+                return x + y;
+            }
+        } else if (res <= 40){
+            if(res == 40){
+                return "XL";
+            } else {
+                String x = "XXX";
+                int second = res % 10;
+                String y = testRome(second);
+                return x + y;
+            }
+        } else if (res <= 50){
+            if(res == 50){
+                return "L";
+            } else {
+                String x = "XL";
+                int second = res % 10;
+                String y = testRome(second);
+                return x + y;
+            }
+        } else if (res <= 60){
+            if(res == 60) {
+                return "LX";
+            } else {
+                String x = "L";
+                int second = res % 10;
+                String y = testRome(second);
+                return x + y;
+            }
+        } else if (res <= 70){
+            if(res == 70){
+                return "LXX";
+            } else {
+                String x = "LX";
+                int second = res % 10;
+                String y = testRome(second);
+                return x + y;
+            }
+        } else if (res <= 80) {
+            if(res == 80){
+                return "LXXX";
+            } else {
+                String x = "LXX";
+                int second = res % 10;
+                String y = testRome(second);
+                return x + y;
+            }
+        } else if (res <= 90) {
+            if(res == 90){
+                return "XC";
+            } else {
+                String x = "LXXX";
+                int second = res % 10;
+                String y = testRome(second);
+                return x + y;
+            }
+        } else if(res == 100){
+            return "C";
+        }
+        return null;
     }
 }
