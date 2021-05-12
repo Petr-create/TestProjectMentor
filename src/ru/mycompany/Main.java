4408e4cafe3d6027866916100b0b55ef6d48227c
petr <petr2768@yandex.ru>
2021-05-12 16:25:27 +0300

    вариант загрузки из файла

--- src/ru/mycompany/Main.java
+++ src/ru/mycompany/Main.java
@@ -1,56 +1,37 @@
 package ru.mycompany;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
     public static Map<String, Integer> map = new HashMap<>();
-    static {
-        map.put("I", 1);
-        map.put("II", 2);
-        map.put("III", 3);
-        map.put("IV", 4);
-        map.put("V", 5);
-        map.put("VI", 6);
-        map.put("VII", 7);
-        map.put("VIII", 8);
-        map.put("IX", 9);
-        map.put("X", 10);
 
-        //и для ответа
-        map.put("XI", 11);
-        map.put("XII", 12);
-        map.put("XIII", 13);
-        map.put("XIV", 14);
-        map.put("XV", 15);
-        map.put("XVI", 16);
-        map.put("XVII", 17);
-        map.put("XVIII", 18);
-        map.put("XIX", 19);
-        map.put("XX", 20);
-    }
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String str = reader.readLine();
-        String[] array = str.split(" ");
-
-        int x, y;
-
-        if((x = test(array[0])) != 0 && (y = test(array[2])) != 0){
-            int res = result(array[1], x, y);
-
-            for(Map.Entry<String, Integer> pair : map.entrySet()){
-                if(pair.getValue() == res){
-                    System.out.println(pair.getKey());
-                }
+        fillMap();
+        while(true) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String str = reader.readLine();
+            if(str.equals("exit")){
+                return;
             }
-        } else {
-            x = Integer.parseInt(array[0]);
-            y = Integer.parseInt(array[2]);
-            System.out.println(result(array[1], x, y));
+            String[] array = str.split(" ");
+
+            int x, y;
+
+            if ((x = test(array[0])) != 0 && (y = test(array[2])) != 0) {
+                int res = result(array[1], x, y);
+
+                for (Map.Entry<String, Integer> pair : map.entrySet()) {
+                    if (pair.getValue() == res) {
+                        System.out.println(pair.getKey());
+                    }
+                }
+            } else {
+                x = Integer.parseInt(array[0]);
+                y = Integer.parseInt(array[2]);
+                System.out.println(result(array[1], x, y));
+            }
         }
     }
 
@@ -76,6 +57,15 @@ public class Main {
             default:
                 System.out.println("?");
                 return 0;
+        }
+    }
+
+    public static void fillMap() throws IOException {
+        try(BufferedReader reader = new BufferedReader(new FileReader("RomanNumerals"))){
+            while(reader.ready()){
+                String[] n = reader.readLine().split(" ");
+                map.put(n[0], Integer.parseInt(n[1]));
+            }
         }
     }
 }
